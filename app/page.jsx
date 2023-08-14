89927cfe6837d650481f9e480ab92ab0aac335dd
Mansi <mansins168@gmail.com>
2023-08-15 00:09:47 +0530

    home page initialised

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,15 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col"> 
+      <h1 className="head_text text-center">
+        Discover & Share
+      </h1>
+       <br className="max-md:hidden"/> {/*//so here we hv a break tag we'will hide on large devices but show in small */}
+       <span className="orange_gradient text-center">AI-Powered Prompts</span>
+       <p className="desc text-center">Promptify is an open source AI prompting tool for modern 
+          world to discover, create and share creative prompts! </p>
+    </section>
   )
 }
 
