184f26f1e2c4b16fdab06ec861163b24c99d28d8
Mansi <mansins168@gmail.com>
2023-08-15 01:07:00 +0530

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -4,9 +4,10 @@ const Home = () => {
     <section className="w-full flex-center flex-col"> 
       <h1 className="head_text text-center">
         Discover & Share
-      </h1>
+      
        <br className="max-md:hidden"/> {/*//so here we hv a break tag we'will hide on large devices but show in small */}
        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+       </h1>
        <p className="desc text-center">Promptify is an open source AI prompting tool for modern 
           world to discover, create and share creative prompts! </p>
     </section>
