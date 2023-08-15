ad1681dc3f7f399445c6b9fba4c9937fccecf728
Mansi <mansins168@gmail.com>
2023-08-16 02:02:15 +0530

    navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+"use client";
+
+import Link from 'next/link'; //help us to navigate between different pages
+import Image from 'next/image'; //automatically optimize the images for us
+import { useState, useEffect } from 'react';
+import {signIn, signOut, UseSession, getProviders } from 'next-auth/react'; //
+
 
 const Nav = () => {
+
+    const isUserLoggedIn = true;
+    const [providers, setProviders] = useState(null);
+
+    useEffect(() => {
+        const setProviders = async () => {
+            const response = await getProviders();
+        }
+    })
+
+
   return (
-    <div>Nav</div>
+    <nav className='flex-between w-full mb-16 pt-3'>
+        <Link href="/" className='flex gap-2 flex-center'>
+            <Image
+                src= "/assets/images/logo.svg"
+                alt="logo"
+                width={30}
+                height={30}
+                className='object-contain'
+            />
+            <p className='logo_text'>Promptify</p>
+        </Link>
+
+        {/* Desktop navigation */}
+        <div className='sm:flex hidden'>
+            {isUserLoggedIn ? (
+                <div className='flex gap-3 md:gap-5'>
+                    <Link href="/create-prompt" className='black_btn'>
+                        Create Post
+                    </Link>
+                    <button type="button" onClick={signOut} className='outline_btn'>
+                        Sign Out
+                    </button>
+                    <Link href="/profile">
+                        <Image 
+                            src="/assets/images/logo.svg"
+                            width={37}
+                            height={37}
+                            className='rounded-full'
+                            alt="profile"
+                        />
+                    </Link>
+                </div>
+            ) : (
+                <>
+
+                </>
+            )}
+        </div>
+    </nav>
   )
 }
 
