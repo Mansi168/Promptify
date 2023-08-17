7025d839c18a5e30b751373cb20490c8fa74d528
Mansi <mansins168@gmail.com>
2023-08-17 18:29:39 +0530

    navbar done for both destop as well as mobile

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -10,11 +10,15 @@ const Nav = () => {
 
     const isUserLoggedIn = true;
     const [providers, setProviders] = useState(null);
+    const [toggleDropdown, setToggleDropdown] = useState(false);
 
     useEffect(() => {
         const setProviders = async () => {
             const response = await getProviders();
+
+            setProviders(response);
         }
+        setProviders();
     })
 
 
@@ -53,7 +57,82 @@ const Nav = () => {
                 </div>
             ) : (
                 <>
+                    {
+                        providers && 
+                        Object.values(providers).map((provider) => (
+                            <button 
+                                type='button'
+                                key={provider.name}
+                                onClick={() => signIn(provider.id)}
+                                className='black_btn'
+                            > 
+                            Sign In 
+                            </button>
+                        ))
+                    }
+                </>
+            )}
+        </div>
 
+        {/* mobile Navigation */}
+        <div className='sm:hidden flex relative'>
+            {isUserLoggedIn ? (
+                <div className='flex'>
+                <Image 
+                    src="/assets/images/logo.svg"
+                    width={37}
+                    height={37}
+                    className='rounded-full'
+                    alt="profile"
+                    onClick={() => setToggleDropdown((prev) => !prev)}
+                />
+
+                {toggleDropdown && 
+                    <div className='dropdown'>
+                        <Link
+                            href='/profile'
+                            className='dropdown_link'
+                            onClick={() => setToggleDropdown(false)}
+                        >
+                        My Profile
+                        </Link>
+                        <Link
+                            href='/create-prompt'
+                            className='dropdown_link'
+                            onClick={() => setToggleDropdown(false)}
+                        >
+                        Create Prompt
+                        </Link>
+                        <button
+                            type="button"
+                            onClick={() => {
+                                setToggleDropdown(false);
+                                signOut();
+                            }}
+                            className='mt-5 w-full black_btn'
+                        >
+                        Sign Out
+                        </button>
+                    </div>
+                }
+
+                </div>
+
+            ) : (
+                <>
+                {
+                        providers && 
+                        Object.values(providers).map((provider) => (
+                            <button 
+                                type='button'
+                                key={provider.name}
+                                onClick={() => signIn(provider.id)}
+                                className='black_btn'
+                            > 
+                            Sign In 
+                            </button>
+                        ))
+                    }
                 </>
             )}
         </div>
