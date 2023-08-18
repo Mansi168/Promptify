88fb4abfe3ae0df472e7a1b547f65212c9173518
Mansi <mansins168@gmail.com>
2023-08-18 16:21:41 +0530

    database initialised

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,1 +1,34 @@
 import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+import {connectToDB} from '@utils/database';
+
+const handler = NextAuth({
+    providers : [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        })
+    ],
+    async session ({session}){
+
+    },
+    async signIn ({profile}){
+        try {
+            await connectToDB();
+
+            //we need to make 2 checks here
+            //1. if user already exists
+
+            //2. if not, create a new user and save it to a database
+
+            return true;
+
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+})
+
+export {handler as GET, handler as POST};
