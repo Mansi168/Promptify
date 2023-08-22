3c8f91ec10331474a0e7602ea946be15bdd822b1
Mansi <mansins168@gmail.com>
2023-08-22 22:17:03 +0530

    next api authenettication route done

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,22 +3,22 @@
 import Link from 'next/link'; //help us to navigate between different pages
 import Image from 'next/image'; //automatically optimize the images for us
 import { useState, useEffect } from 'react';
-import {signIn, signOut, UseSession, getProviders } from 'next-auth/react'; //
+import {signIn, signOut, useSession, getProviders } from 'next-auth/react'; //
 
 
 const Nav = () => {
 
-    const isUserLoggedIn = true;
+    const {data : session} = useSession();
     const [providers, setProviders] = useState(null);
     const [toggleDropdown, setToggleDropdown] = useState(false);
 
     useEffect(() => {
-        const setProviders = async () => {
+        const setUpProviders = async () => {
             const response = await getProviders();
 
             setProviders(response);
         }
-        setProviders();
+        setUpProviders();
     })
 
 
@@ -37,7 +37,7 @@ const Nav = () => {
 
         {/* Desktop navigation */}
         <div className='sm:flex hidden'>
-            {isUserLoggedIn ? (
+            {session?.user ? (
                 <div className='flex gap-3 md:gap-5'>
                     <Link href="/create-prompt" className='black_btn'>
                         Create Post
@@ -47,7 +47,7 @@ const Nav = () => {
                     </button>
                     <Link href="/profile">
                         <Image 
-                            src="/assets/images/logo.svg"
+                            src={session?.user.image}
                             width={37}
                             height={37}
                             className='rounded-full'
@@ -76,10 +76,10 @@ const Nav = () => {
 
         {/* mobile Navigation */}
         <div className='sm:hidden flex relative'>
-            {isUserLoggedIn ? (
+            {session?.user ? (
                 <div className='flex'>
                 <Image 
-                    src="/assets/images/logo.svg"
+                    src={session?.user.image}
                     width={37}
                     height={37}
                     className='rounded-full'
